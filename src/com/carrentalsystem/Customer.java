85e8f728d4de2dbcf53952b077b6f558e776fb60
AKASH KUMAR <akakash201@gmail.com>
2024-11-25 19:17:03 +0530

    initial commit

---
+++ src/com/carrentalsystem/Customer.java
@@ -0,0 +1,26 @@
+package com.carrentalsystem;
+
+public class Customer 
+{
+	private String customerId;
+	private String name;
+	
+	public Customer(String customerId,String name)
+	{
+		this.name=name;
+		this.customerId=customerId; 
+	}
+	
+	public String getCustomerId()
+	{
+		return customerId;
+	}
+
+	public String getName()
+	{
+		return name;
+		
+	}
+	
+	
+}
